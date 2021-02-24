8ca5132a8d70ab8035ef176cc29a82be23c113cc
HeoInSung <isheo2001@naver.com>
2021-02-24 18:58:57 +0900

    구조체 이용

--- 구현_왕실의나이트_115pg/main.c
+++ 구현_왕실의나이트_115pg/main.c
@@ -1,7 +1,15 @@
 #include <stdio.h>
 #include <stdlib.h>
+
+// typedef 선언, 좌표 표시
+typedef struct
+{
+    int x;
+    int y;
+}Point;
+
 // 현재 위치와 나이트 움직임을 입력해줬을 때 체스판을 벗어나면 0, 벗어나지 않으면 1을 리턴하는 함수
-int move(int l[], int m[]);
+int move(Point start, Point mv);
 
 int main()
 {
@@ -9,19 +17,28 @@ int main()
     int cnt = 0;
 
     // 나이트의 움직임
-    int mv[8][2] = {{-2, -1}, {-2, 1}, {-1, -2}, {-1, 2}, {1, -2}, {1, 2}, {2, -1}, {2, 1}};
+    Point mv[8] = {
+        {-2, -1},
+        {-2, +1},
+        {-1, -2},
+        {-1, +2},
+        {+1, -2},
+        {+1, +2},
+        {+2, -1},
+        {+2, +1}
+    };
 
     // 초기 위치 입력받기
     char * location;
     scanf("%s", location);
-    int x = location[0] - ('a'-1);
-    int y = location[1] - '0';
-    int l[2] = {x, y};
+    Point start;
+    start.x = location[0] - ('a' - 1);
+    start.y = location[1] - '0';
 
     // 8가지 움직임에 대해서 나이트의 위치 확인
     for(int i = 0; i < 8; i++)
     {
-        if(move(l, mv[i]) == 1)
+        if(move(start, mv[i]) == 1)
             cnt ++;
     }
 
@@ -29,10 +46,10 @@ int main()
     return 0;
 }
 
-int move(int *l, int *m)
+int move(Point start, Point mv)
 {
-    int x = l[0] + m[0];
-    int y = l[1] + m[1];
+    int x = start.x + mv.x;
+    int y = start.y + mv.y;
     if(x >= 1 && x <= 8)
     {
         if(y >= 1 && y <= 8)
